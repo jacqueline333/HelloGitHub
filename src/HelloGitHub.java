5c6040ebe9f94b71cbec460c232d6746b3fcfde5
JACI\Besitzer <jacqueline.perl@edu.campus02.at>
2016-09-29 14:16:44 +0200

    kleine Änderung vom 2ten Entwickler

--- src/HelloGitHub.java
+++ src/HelloGitHub.java
@@ -2,7 +2,7 @@
 public class HelloGitHub {
 
 	public static void main(String[] args) {
-		System.out.println ("Hello GitHub");
+		System.out.println ("Hello GitHub, I' developer 2!");
 		// TODO Auto-generated method stub
 
 	}
