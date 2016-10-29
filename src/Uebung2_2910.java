67e35482533536c1fe3419df56441bab3dc392dd
JACI\Besitzer <jacqueline.perl@edu.campus02.at>
2016-10-29 14:54:41 +0200

    Übung 2 29.10.

---
+++ src/Uebung2_2910.java
@@ -0,0 +1,15 @@
+
+public class Uebung2_2910 {
+
+	public static void main(String[] args) {
+		
+		
+		int fuellStand =50;
+		
+		if (fuellStand <10) {
+			System.out.println("Tank wird leer!");
+		}
+
+	}
+
+}
