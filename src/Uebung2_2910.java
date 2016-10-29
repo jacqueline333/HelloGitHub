511a9a2c2df0ca3b3006625d2cefd09cb61dcb66
JACI\Besitzer <jacqueline.perl@edu.campus02.at>
2016-10-29 15:27:34 +0200

    Übung 2

--- src/Uebung2_2910.java
+++ src/Uebung2_2910.java
@@ -3,13 +3,37 @@ public class Uebung2_2910 {
 
 	public static void main(String[] args) {
 		
+		//  Metropolis 
 		
-		int fuellStand =50;
-		
-		if (fuellStand <10) {
-			System.out.println("Tank wird leer!");
-		}
-
+	boolean result = isMetropolis(true,1_000, 100);
+	
+	if(result){
+	System.out.println("Diese Stadt ist eine Metropole: ");	
+	} else { System.out.println("Diese Stadt ist uncool");
 	}
+	}
+	
+	
+		public static boolean isMetropolis(	boolean isCapital, 
+											int inhabitans, 
+											double taxPerPersonAndMonth)
+		{
+			
+			double taxes = inhabitans * taxPerPersonAndMonth *12;
+			
+			if(inhabitans >100_000 && isCapital == true
+				|| taxes >720_000_000 && inhabitans > 200_000) {
+				return true;
+				} else {
+				return false;}
+			}
+	}
+		
 
-}
+//		boolean resultA = capital && inhabitans > 100_000;
+//		boolean resultB = taxes > 720_000_000 && inhabitans > 200_000;
+//		return resultA || resultB;	
+	
+ 
+
+
