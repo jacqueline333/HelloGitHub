4ec153a2b99d63af009bde53df1c9c8f2634947c
JACI\Besitzer <jacqueline.perl@edu.campus02.at>
2016-11-12 08:33:01 +0100

    Übung 1 12.11.2016

---
+++ src/Uebung1_1211.java
@@ -0,0 +1,22 @@
+
+public class Uebung1_1211 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int i = 0;
+		 
+		while (i<10){
+			i++;
+			if (i % 2 ==0){
+				continue;
+				
+			}
+			
+			System.out.println(i);
+			
+		}
+	}
+
+}
