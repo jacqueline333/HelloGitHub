5510b780aa3d2f37ecabff2ebce2613befa9d38c
JACI\Besitzer <jacqueline.perl@edu.campus02.at>
2016-11-12 10:35:24 +0100

    12.11.

---
+++ src/Uebung2_1211.java
@@ -0,0 +1,36 @@
+
+public class Uebung2_1211 {
+
+	public static void main(String[] args) {
+		
+	//Array Demo
+		
+	
+		int [] array1 = {5,6,7};
+		
+		int [] array2 = new int [3];
+				
+		array2[0] = 2;
+		array2[1] = 3;
+		array2[2] = 4;
+		
+		System.out.print("+ ");
+		System.out.print((array1[0] + array2[0]) + " ,");
+		System.out.print((array1[1] + array2[1]) + " ,");
+		System.out.println((array1[2] + array2[2]));
+		
+		System.out.print("* ");
+		System.out.print((array1[0] * array2[0]) + " ,");
+		System.out.print((array1[1] * array2[1]) + " ,");
+		System.out.println((array1[2] * array2[2]));
+		
+			
+		
+		
+				
+
+		
+
+	}
+
+}
---
+++ src/Uebung3_1211.java
@@ -0,0 +1,22 @@
+
+public class Uebung3_1211 {
+
+	public static void main(String[] args) {
+		
+		//Array Demo 2
+		
+		String [] Monate = {"Jänner", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"};
+		
+		
+		
+		for(int m = 0; m < Monate.length; m++){
+			System.out.println(Monate[m]+ "[" +(m+1)+ "]");
+			
+		}
+			
+		
+
+		
+	}
+
+}
